11bbc5d6a2ba99d5e87de7f9d32880f0f8d3e8c0
emilioc2 <emiliochanderdutt9@gmail.com>
2026-03-25 16:31:04 +0200

    Header dark brown background

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ export function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="bg-surface-dark/80 text-white">
+    <footer className="bg-surface-dark text-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-14">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
 
